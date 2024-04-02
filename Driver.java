805ab483467e9e71e82da19168a75321199040de
Alan Linto <alanlinto@gmail.com>
2024-04-02 15:08:20 +1100

    Backend updates
    Some fixes in response to the implementation of the RSA algorithm.

--- Driver.java
+++ Driver.java
@@ -1,13 +1,23 @@
-import java.math.BigInteger;
+import java.math.BigDecimal;
 
 public class Driver {
     public static void main(String[] args) {
-        String_Bits o = new String_Bits();
-        String binary = o.stringToBits(
-                "NY BHBS BJSBJB SJHSJH SJHJHS JSHJHJ SHJHJ SJHJH SHJH SJHJH SJHJH SJHJH SJHJHJHJ JHJHJS HJHJHS JUHJHJS ");
-        System.out.println(binary);
-        BigInteger decimalNumber = new BigInteger(binary, 2);
-
-        System.out.println("Decimal representation: " + decimalNumber);
+        /*
+         * String_Bits o = new String_Bits();
+         * String binary = o.stringToBits(
+         * "NY BHBS BJSBJB SJHSJH SJHJHS JSHJHJ SHJHJ SJHJH SHJH SJHJH SJHJH SJHJH SJHJHJHJ JHJHJS HJHJHS JUHJHJS "
+         * );
+         * System.out.println(binary);
+         * BigInteger decimalNumber = new BigInteger(binary, 2);
+         * 
+         * System.out.println("Decimal representation: " + decimalNumber);
+         */
+        BigDecimal p = new BigDecimal("120");
+        BigDecimal q = new BigDecimal("100");
+        BigDecimal e = new BigDecimal("4");
+        RSA obj = new RSA(p, q, e, "Hello");
+        System.out.println(obj.getN());
+        System.out.println(obj.getR());
+        System.out.println(obj.getPrivateKey());
     }
 }
--- RSA.java
+++ RSA.java
@@ -1,12 +1,58 @@
 import java.math.BigDecimal;
-import java.math.BigInteger;
+import java.math.RoundingMode;
 
 public class RSA {
-    private BigInteger p, q, n;
-    private BigDecimal e, d;
+    private BigDecimal p, q, n, r, e, d;
     private String m, c;
 
     public RSA() {
+        this.p = null;
+        this.q = null;
+        this.n = null;
+        this.r = null;
+        this.e = null;
+        this.d = null;
+    }
 
+    public RSA(BigDecimal p, BigDecimal q, BigDecimal e, String m) {
+        this.p = p;
+        this.q = q;
+        this.e = e;
+        this.m = m;
+        this.n = this.p.multiply(this.q);
+        this.r = this.p.subtract(BigDecimal.ONE).multiply(this.q.subtract(BigDecimal.ONE));
+        this.d = this.modularInverse(this.e, this.r);
+        // System.out.println("So far all good!!!!");
+    }
+
+    private BigDecimal modularInverse(BigDecimal e, BigDecimal r) {
+        // Ensure that e and r are positive
+        e = e.abs();
+        r = r.abs();
+
+        // Calculate the modular inverse using the remainder
+        BigDecimal inverse = new BigDecimal("1").divide(e);
+        // System.out.println(inverse);
+        return inverse.setScale(r.scale(), RoundingMode.HALF_UP).remainder(r);
+    }
+
+    public void encrypt() {
+
+    }
+
+    public void decrypt() {
+
+    }
+
+    public BigDecimal getN() {
+        return this.n;
+    }
+
+    public BigDecimal getR() {
+        return this.r;
+    }
+
+    public BigDecimal getPrivateKey() {
+        return this.d;
     }
 }
